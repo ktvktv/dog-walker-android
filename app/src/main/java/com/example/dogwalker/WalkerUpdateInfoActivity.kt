37218f70bdedb37582a5a29fee49c602b7d3176c
Kevin Tigravictor <kevintigravictor30@gmail.com>
2020-02-20 15:50:53 +0700

    Fix: Perbarui Walker

--- app/src/main/java/com/example/dogwalker/WalkerUpdateInfoActivity.kt
+++ app/src/main/java/com/example/dogwalker/WalkerUpdateInfoActivity.kt
@@ -1,9 +1,12 @@
 package com.example.dogwalker
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -33,7 +36,7 @@ class WalkerUpdateInfoActivity : AppCompatActivity() {
 
         val walkerUpdateBinding = ActivityWalkerRegisterBinding.inflate(LayoutInflater.from(this))
 
-        walkerUpdateBinding.walkerRegisterTitle.text = "Perbarui walker"
+        walkerUpdateBinding.walkerRegisterTitle.text = "PERBARUI WALKER"
         walkerUpdateBinding.registerButton.text = "Ubah"
 
         coroutineScope.launch {
@@ -67,6 +70,68 @@ class WalkerUpdateInfoActivity : AppCompatActivity() {
                 Toast.makeText(this, "Terjadi kesalahan, silahkan coba lagi", Toast.LENGTH_SHORT).show()
             }
         })
+
+        val alertDialog = AlertDialog.Builder(ContextThemeWrapper(this, android.R.style.Theme_Material_Light_Dialog_Alert))
+            .setTitle("Informasi")
+            .setPositiveButton("OK") { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }.create()
+
+        walkerUpdateBinding.infoButtonMaxDog.setOnTouchListener { view, motionEvent ->
+            when(motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    alertDialog.setMessage("Jangka berat anjing pada umumnya:\n" +
+                            "5.5 kg untuk anjing paling kecil(toy)\n" +
+                            "5.5 - 11 kg untuk anjing kecil\n" +
+                            "11 - 23 kg untuk anjing sedang\n" +
+                            "23 - 45 kg untuk anjing besar\n" +
+                            "45 kg keatas untuk anjing sangat besar")
+                    alertDialog.show()
+
+                    true
+                }
+                else -> true
+            }
+        }
+
+        walkerUpdateBinding.infoButtonMaxDist.setOnTouchListener { view, motionEvent ->
+            when(motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    alertDialog.setMessage("Field ini menentukan maksimal jarak pengambilan anjing yang anda ingini\n\n" +
+                            "Pesanan-pesanan yang akan anda terima pasti kurang dari/sama dengan jarak yang anda isi pada field ini")
+                    alertDialog.show()
+
+                    true
+                }
+                else -> true
+            }
+        }
+
+        walkerUpdateBinding.infoButtonMaxDuration.setOnTouchListener { view, motionEvent ->
+            when(motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    alertDialog.setMessage("Field ini menentukan maksimal durasi untuk menjalankan anjing yang anda ingini\n\n" +
+                            "Pesanan-pesanan yang akan anda terima pasti kurang dari/sama dengan durasi yang anda isi pada field ini")
+                    alertDialog.show()
+
+                    true
+                }
+                else -> true
+            }
+        }
+
+        walkerUpdateBinding.infoButtonPricing.setOnTouchListener { view, motionEvent ->
+            when(motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    alertDialog.setMessage("Field ini menentukan harga/jam untuk kompensasi anda dalam menjalankan anjing\n\n" +
+                            "Field ini akan ditunjukkan kepada customer dalam pemilihan walker")
+                    alertDialog.show()
+
+                    true
+                }
+                else -> true
+            }
+        }
 
         walkerUpdateBinding.registerButton.setOnClickListener {
             var maxDogSize: Int
